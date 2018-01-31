76b2a3ce42a1f524a5b28fdaf059970aa5c7d12f
Brian van den Broek <vanden@gmail.com>
2018-01-31 11:41:51 -0800

    Rename files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 
-  validates: :username, :password, :email, presence:true
+  validates :username, :password, :email, presence:true
   validates :password, length: { minimum: 6 }
 end
