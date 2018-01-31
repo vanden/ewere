883c60c524cd3cd64f80a5282eadecce2f1fa19a
Brian van den Broek <vanden@gmail.com>
2018-01-31 10:53:15 -0800

    Add basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+
+  validates: :username, :password, :email, presence:true
+  validates :password, length: { minimum: 6 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create, :update, :destroy, :show]
+  end
 
   root "static_pages#root"
 end
