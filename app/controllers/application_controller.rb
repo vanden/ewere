456e7fe8673bcaccae26491e2c2cc2c1c0c7821c
Brian van den Broek <vanden@gmail.com>
2018-01-31 14:06:55 -0800

    Add basis of user assets for backend auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,35 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_user, :logged_in?
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+
+  def logout
+    current_user.reset_session_token!
+    @current_user = nil
+    session[:session_token] = nil
+  end
+
+
+  def logged_in?
+    !!current_user
+  end
+
+
+  def current_user
+    unless session[:session_token] then return nil end
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+
+  def require_logged_on
+    unless current_user
+      render json: { base: ['invalid credentials'] }, status: 401
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,57 @@
 class User < ApplicationRecord
 
   validates :username, :password, :email, presence:true
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+  attr_reader :password   # Do I need this?
+
+  # has_many :tasks
+  # has_many :teams
+  # has_many :projects
+  # can I have may task and have many task through projects?
+
+
+  def self.find_by_credentials(email, password)
+    user = User.find_(email: email)
+    unless user then return nil end
+
+    if user.is_password? then return user end
+
+    return nil
+  end
+
+
+  def
+    @password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    save!
+    self.session_token
+  end
+
+
+  def generate_unique_session_token
+    loop do
+      self.session_token = SecureRandom::urlsafe_base64
+      unless User.find_by(session_token: self.session_token) then break end
+    end
+    self.session_token
+  end
 end
