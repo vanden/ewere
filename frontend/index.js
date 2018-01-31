dc6dab65153684bb73511f6e7b7b4a3ef795a778
Brian van den Broek <vanden@gmail.com>
2018-01-31 10:56:48 -0800

    Add canges from setup_front_end_skeleton

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Hello erewe world!</h1>, root);
+});
