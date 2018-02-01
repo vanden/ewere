313273e1b797ad04ab3df56253fba568ebe52abe
Brian van den Broek <vanden@gmail.com>
2018-01-31 18:08:08 -0800

    Fix typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  validates :username, :password, :email, presence:true
+  validates :username, :password_digest, :session_token, :email, presence: true
   validates :password, length: { minimum: 6 }, allow_nil: true
 
   after_initialize :ensure_session_token
@@ -13,10 +13,10 @@ class User < ApplicationRecord
 
 
   def self.find_by_credentials(email, password)
-    user = User.find_(email: email)
+    user = User.find_by(email: email)
     unless user then return nil end
 
-    if user.is_password? then return user end
+    if user.is_password?(password) then return user end
 
     return nil
   end
