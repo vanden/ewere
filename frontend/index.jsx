5adb1d6297b14461344325cc2d0dc60bb80feba8
Brian van den Broek <vanden@gmail.com>
2018-01-31 23:44:25 -0800

    Adapt early draft of store to reducers

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore()
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Hello ewere world!</h1>, root);
 });
