e687ec3624d58444a9fb2bb90b9f38078b3085a7
Brian van den Broek <vanden@gmail.com>
2018-01-31 23:33:19 -0800

    Add errors reducer and wrap sessionErrors reducer

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,30 @@
+import { combineReducers } from 'redux'
+
+import { RECEIVE_SESSION_ERRORS,
+         RECEIVE_CURRENT_USER } from '../actions/session_actions'
+
+// May ultimately want to separate out sessionErrorsReducer and
+// errorsReducer but for now, stem the proliferation of file.
+
+
+const sessionErrorsReducer = (state = [], action) => {
+
+  Object.freeze(state)
+
+  switch (action.type) {
+
+  case RECEIVE_SESSION_ERRORS:
+    return action.errors
+
+  case RECEIVE_CURRENT_USER:
+    return null
+
+  default:
+    return state
+  }
+};
+
+
+export default errorsReducer = combineReducers(
+  {session: sessionErrorsReducer}
+)
