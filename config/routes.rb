4a84bd619bbb359f896d0d25d787fd885680a9ee
Brian van den Broek <vanden@gmail.com>
2018-01-31 18:05:35 -0800

    Switch route from user to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
-    resource :user, only: [:create, :update, :destroy, :show]
+    resources :users, only: [:create, :update, :destroy, :show]
     resource :session, only: [:create, :destroy, :show]
   end
 
