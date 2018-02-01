e194b18f9cff83beca62ef3e4e837f9b2abee448
Brian van den Broek <vanden@gmail.com>
2018-01-31 23:44:25 -0800

    Adapt early draft of store to reducers

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -15,25 +15,43 @@
 // You should have received a copy of the GNU General Public License
 // along with ewere.  If not, see <http://www.gnu.org/licenses/>.
 
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 
-// Commented out until developed. This all included early to ensure
-// that redux-logger doesn't slip into production by mistake.
-//
-//import rootReducer from '../reducers/root_reducer';
 
-const middlewares = [thunk];
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger' // FixMe Remove this in favour of
+                                  // require below once in production
 
-if (process.env.NODE_ENV !== 'production') {
-  // must use 'require' (import only allowed at top of file)
-  const { logger } = require('redux-logger');
-// Still commented out for extra caution. Must investigate what defines process.env.NODE_ENV
-//  middlewares.push(logger);
+const thunk = ({ dispatch, getState }) => next => action => {
+  if (typeof action === 'function') {
+    return action(dispatch, getState)
+  }
+  return next(action)
 }
 
-// const configureStore = (preloadedState = {}) => (
-//   createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
-// );
 
-// export default configureStore;
+
+import rootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk]
+middlewares.push(logger)  // FixMe Remove once in production
+
+// Commented out until we are ready for production. This all included
+// early to ensure that redux-logger doesn't slip into production by
+// mistake.
+//
+// if (process.env.NODE_ENV !== 'production') {
+//   // must use 'require' (import only allowed at top of file)
+//   const { logger } = require('redux-logger');
+// // Still commented out for extra caution. Must investigate what defines process.env.NODE_ENV
+// //  middlewares.push(logger);
+// }
+
+const configureStore = (preloadedState = {}) => (
+  createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(...middlewares))
+  )
+);
+
+export default configureStore;
