74d5606719fc1b68e1a322fd4bc81e00cceb3562
Brian van den Broek <vanden@gmail.com>
2018-02-01 17:49:07 -0800

    Add protection for routes
    As yet, only the AuthRoute protecting signup and login from being shown
    to logged in users who type the url are used.
    
    In parallel, ProtectedRoute has been added, but there is nothing to
    protect, as yet.

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,14 +3,16 @@ import { Link, Route, Switch } from 'react-router-dom'
 
 import SessionFormContainer from './session_form/session_form_container'
 
+import { AuthRoute, ProtectedRoute } from '../util/route_util'
+
 const App = () => (
   <div>
     <Link to="/" className="header-link">
       <h1>Hello, ewere world from react-redux land!</h1>
     </Link>
     <Switch>
-      <Route path="/login" component={SessionFormContainer} />
-      <Route path="/signup" component={SessionFormContainer} />
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
     </Switch>
   </div>
 )
