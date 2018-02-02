07ce4f46d57059126a590c26c81e1ec896341ffe
Brian van den Broek <vanden@gmail.com>
2018-02-01 16:27:27 -0800

    Convert to rendering from react

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => (
   <div>
-    <h1>Hello, ewere world from react-redux land!</h1>
+    <Link to="/" className="header-link">
+      <h1>Hello, ewere world from react-redux land!</h1>
+    </Link>
   </div>
 )
 
