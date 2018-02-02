cc49c3692b4cc9b08b826097fb11bc3c6e07f5d5
Brian van den Broek <vanden@gmail.com>
2018-02-02 10:25:10 -0800

    Add navbar to all routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link, Route, Switch } from 'react-router-dom'
 
 import SessionFormContainer from './session_form/session_form_container'
+import NavBarContainer from './navbar/navbar_container'
 
 import { AuthRoute, ProtectedRoute } from '../util/route_util'
 
 const App = () => (
   <div>
+    <Route path="/" component={NavBarContainer}/>
     <Link to="/" className="header-link">
       <h1>Hello, ewere world from react-redux land!</h1>
     </Link>
