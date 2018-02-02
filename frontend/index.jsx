2e9a25d782067979960f3778cf4d11d04c7d501e
Brian van den Broek <vanden@gmail.com>
2018-02-01 22:00:28 -0800

    Move window (testing) assignment to end

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -8,16 +8,16 @@ import Root from './components/root'
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore()
 
-  // FixMe Here for testing
-  window.getState = store.getState
-  window.dispatch = store.dispatch
-
   const root = document.getElementById('root')
   ReactDOM.render(<Root store={ store } />, root)
 });
 
 
 import { login, logout, signup } from './util/session_api_util'
+
+// FixMe Here for testing
+window.getState = store.getState
+window.dispatch = store.dispatch
 
 window.login = login
 window.logout = logout
