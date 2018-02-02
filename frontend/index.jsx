66cd1d6967c9ea7314846a722e695b55ef4f8f6a
Brian van den Broek <vanden@gmail.com>
2018-02-01 22:01:55 -0800

    Configure preloaded state from current user

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -8,6 +8,14 @@ import Root from './components/root'
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore()
 
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } }
+    store = configureStore(preloadedState)
+    delete window.currentUser
+  } else {
+    store = configureStore()
+  }
+  
   const root = document.getElementById('root')
   ReactDOM.render(<Root store={ store } />, root)
 });
