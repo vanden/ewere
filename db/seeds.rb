bc09278d69cb8d4e0118c6fb6543de78f193c074
Brian van den Broek <vanden@gmail.com>
2018-02-03 15:16:51 -0800

    Expand user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,12 @@
 
 user1 = User.create(email: "guest@example.com", username: "guest", password:"password")
 user2 = User.create(email: "vanden@gmail.com", username: "Brian", password:"123456")
+user3 = User.create(email: "pat@example..com", username: "Pat", password:"123456")
+user4 = User.create(email: "sam@example.com", username: "Sam", password:"123456")
+user5 = User.create(email: "chris@example.com", username: "Chris", password:"123456")
+user6 = User.create(email: "hillary@example.com", username: "Hillary", password:"123456")
+user7 = User.create(email: "alex@example.com", username: "Alex", password:"123456")
+user8 = User.create(email: "drew@example.com", username: "Drew", password:"123456")
+
+
+
