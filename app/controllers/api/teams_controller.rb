3e948a0d27fb4fad1125a5d7f175c8ab1608afbb
Brian van den Broek <vanden@gmail.com>
2018-02-03 15:32:30 -0800

    Add teams api controller

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,16 +1,54 @@
 class Api::TeamsController < ApplicationController
+
+  before_action :require_logged_on
+  
   def create
+    @team = Team.new(team_params)
+    @team.creator_id = current_user.id
+
+    if @team.save
+# Temporarily commented out as TeamMembership.create does not yet exist      
+#      TeamMembership.create(member_id: current_user.id, team_id: @team.id)
+      render :show
+    else
+      render json: @team.errors.full_messages, status: 422
+    end
   end
 
-  def update
-  end
-
-  def show
+  
+  def destroy
+    @team = Team.find(params[:id])
+    if @team.destroy
+      render "api/teams/show"
+    else
+      render plain: "That team does not exist to be destroyed."
+    end
   end
 
   def index
+    @teams = Team.all
   end
 
-  def destroy
+  
+  def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes(team_params)
+      render :show
+    else
+      render json: @team.errors.full_messages, status: 422
+    end
+  end
+
+  
+  def show
+    @team = Team.find(params[:id])
+  end
+
+  
+  private
+
+  
+  def team_params
+    params.require(:team).permit(:name, :description)
   end
 end
