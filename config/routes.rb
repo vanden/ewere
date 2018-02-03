fddee2d400acb45e8c9db48255187ed590b19cf0
Brian van den Broek <vanden@gmail.com>
2018-02-03 15:05:37 -0800

    Add skeleton teams controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :update, :destroy, :show]
     resource :session, only: [:create, :destroy, :show]
+    resources :teams, except: [:new, :edit]
   end
 
   root to: "static_pages#root"
