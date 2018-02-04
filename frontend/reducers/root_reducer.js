b9b7db68d5a2517be4dd68eb3884d614a008de98
Brian van den Broek <vanden@gmail.com>
2018-02-04 01:19:08 -0800

    Add team reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import session from './session_reducer';
 import errors from './errors_reducer';
+import teams from './team_reducer'
 
 const rootReducer = combineReducers({
   session,
   errors,
+  teams
 });
 
 export default rootReducer;
---
+++ frontend/reducers/team_reducer.js
@@ -0,0 +1,29 @@
+import { RECEIVE_TEAM,
+         RECEIVE_ALL_TEAMS,
+         REMOVE_TEAM } from '../actions/team_actions'
+
+const defaultState = Object.freeze({})
+
+const teamReducer = (state = defaultState, action) => {
+
+  Object.freeze(state)
+
+  switch (action.type) {
+
+  case RECEIVE_TEAM:
+    return Object.assign({}, state, { action.team })
+
+  case RECEIVE_ALL_TEAMS:
+    return Object.assign({}, action.teams)
+
+  case REMOVE_TEAM:
+    let newState = Object.assign({}, state)
+    delete newState[action.team.id]
+    return newState                         
+                         
+  default:
+    return state
+  }
+}
+
+export default teamReducer
