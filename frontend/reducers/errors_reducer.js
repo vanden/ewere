fcb470fafb40e7c08a6463363d1486cac0b92bc2
Brian van den Broek <vanden@gmail.com>
2018-02-04 11:53:57 -0800

    Reorganize reducers

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,54 +1,8 @@
 import { combineReducers } from 'redux'
 
-import { RECEIVE_SESSION_ERRORS,
-         RECEIVE_CURRENT_USER } from '../actions/session_actions'
-import { RECEIVE_TEAM,
-         RECEIVE_ALL_TEAMS,
-         REMOVE_TEAM,
-//         REMOVE_TEAM_ERRORS, // Needed? ThinkMore
-         RECEIVE_TEAM_ERRORS } from '../actions/team_actions'
+import { sessionErrorsReducer } from './session_reducer'
+import { teamErrorsReducer } from './team_reducer'
 
-// May ultimately want to separate out sessionErrorsReducer and
-// errorsReducer but for now, stem the proliferation of file.
-
-
-const sessionErrorsReducer = (state = [], action) => {
-
-  Object.freeze(state)
-
-  switch (action.type) {
-
-  case RECEIVE_SESSION_ERRORS:
-    return action.errors
-
-  case RECEIVE_CURRENT_USER:
-    return [] // Was return null. Does it matter? FixMe
-
-  default:
-    return state
-  }
-};
-
-
-const teamErrorsReducer = (state = [], action) => {
-
-  Object.freeze(state)
-
-  switch(action.type) {
-
-  case RECEIVE_TEAM:
-  case RECEIVE_ALL_TEAMS:
-  case REMOVE_TEAM:
-//  case REMOVE_TEAM_ERRORS:  // Needed? ThinkMore
-    return []
-
-  case RECEIVE_TEAM_ERRORS:
-    return action.errors;
-
-  default:
-    return state
-  }
-}
 
 export default combineReducers(
   {session: sessionErrorsReducer,
