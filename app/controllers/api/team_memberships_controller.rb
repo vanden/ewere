4e185ece5e083641ffc656b69b7f06b8f7949611
Brian van den Broek <vanden@gmail.com>
2018-02-04 13:04:41 -0800

    Add first draft of team membership controller

--- app/controllers/api/team_memberships_controller.rb
+++ app/controllers/api/team_memberships_controller.rb
@@ -1,7 +1,34 @@
 class Api::TeamMembershipsController < ApplicationController
+
   def create
+    @team_membership = TeamMembership.new(team_membership_params)
+
+    if @team_membership.save
+      @team = team_membership.team
+      # Show a team, not all teams, right? # ThinkMore
+      render "api/teams/show"
+    else
+      render json: @team_membership.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @team_membership = TeamMembership.find(params[:id])
+
+    if @team_membership.destroy
+      # Where to go, now? # Question
+      # placeholder until I figure it out
+      redirect_to root_url
+    else
+      render plain: "You cannot destroy a non-existent membership"
+    end
+
+  end
+
+
+  private
+
+  def team_membership_params
+    params.require(:team_membership).permit(:team_id, :member_id)
   end
 end
