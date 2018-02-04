a03a5fed6074571f3468d256ee84e87deffced2f
Brian van den Broek <vanden@gmail.com>
2018-02-04 12:38:47 -0800

    Add team_membership routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :update, :destroy, :show]
     resource :session, only: [:create, :destroy, :show]
     resources :teams, except: [:new, :edit]
+    resources :team_memberships, only: [:create, :show, :destroy]
   end
 
   root to: "static_pages#root"
