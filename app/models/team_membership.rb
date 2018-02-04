af8af88fcb268d4934935d697cda0d4f0f636272
Brian van den Broek <vanden@gmail.com>
2018-02-04 15:28:04 -0800

    Add validation for team_membership

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -1,6 +1,7 @@
 class TeamMembership < ApplicationRecord
 
   validates :team_id, :member_id, presence: true
+  validates_uniqueness_of :member_id, scope: :team_id
 
   belongs_to :member,
     class_name: 'User',
