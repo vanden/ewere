f005c943750091c99a9957cc982f65acae9fcea5
Brian van den Broek <vanden@gmail.com>
2018-02-04 18:45:39 -0800

    Add desired Project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,11 @@
 Rails.application.routes.draw do
-  
-  namespace :api do
-    get 'projects/create'
-  end
-
-  namespace :api do
-    get 'projects/update'
-  end
-
-  namespace :api do
-    get 'projects/show'
-  end
-
-  namespace :api do
-    get 'projects/index'
-  end
-
-  namespace :api do
-    get 'projects/destroy'
-  end
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :update, :destroy, :show]
     resource :session, only: [:create, :destroy, :show]
     resources :teams, except: [:new, :edit]
     resources :team_memberships, only: [:create, :show, :destroy]
+    resources :projects, except: [:new, :edit]
   end
 
   root to: "static_pages#root"
