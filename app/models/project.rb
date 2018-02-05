1204df17b0eb109f4822dd0fbf7f7987304051c7
Brian van den Broek <vanden@gmail.com>
2018-02-04 17:41:08 -0800

    Add first draft of Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,24 @@
 class Project < ApplicationRecord
+
+  validates :creator_id, :team_id, :description, presence: true
+
+  # It would be better to scope the uniqueness to the team, wouldn't
+  # it? # ThinkMore
+  validates :name, presence: true, uniqueness: true
+
+  belongs_to :creator,
+             foreign_key: :creator_id,
+             class_name: :User
+
+  belongs_to :team,
+             foreign_key: :team_id,
+             class_name: :Team
+
+  # Should have_many Tasks once Tasks exist
+
+
+  has_many :members,
+           through: :team,
+           source: :members
+
 end
