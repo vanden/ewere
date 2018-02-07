721aa52c96c33c16596b31aab57024e1300b191c
Brian van den Broek <vanden@gmail.com>
2018-02-06 21:11:58 -0800

    Add members reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 
 import { teamReducer } from './team_reducer'
+import members from './member_reducer'
 import projects from './project_reducer'
 
 
 const entitiesReducer = combineReducers({
   teams: teamReducer,
+  members,
   projects,  
 })
 
