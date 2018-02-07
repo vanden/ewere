e74f36f7afd554112941a5b63fe27288b10d765f
Brian van den Broek <vanden@gmail.com>
2018-02-07 00:05:56 -0800

    Fix memebrs reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,13 +1,13 @@
 import { combineReducers } from 'redux'
 
 import { teamReducer } from './team_reducer'
-import members from './member_reducer'
+import { membersReducer } from './member_reducer'
 import projects from './project_reducer'
 
 
 const entitiesReducer = combineReducers({
   teams: teamReducer,
-  members,
+  members: membersReducer,
   projects,  
 })
 
