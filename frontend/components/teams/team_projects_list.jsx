979a8f8755917120bed93be79afb9cb85c8d4f48
Brian van den Broek <vanden@gmail.com>
2018-02-08 10:52:41 -0800

    Add fully commented out first stab at team project list component

---
+++ frontend/components/teams/team_projects_list.jsx
@@ -0,0 +1,38 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom'
+
+
+// import TeamIndexItem from './team_index_item';
+// //import TeamFormContainer from './team_form_container';
+
+
+
+// const TeamProjectsList = ({ projects }) => {
+
+
+//   return (
+//     <div className="team-projects-list">
+      
+//     </div>
+//   )
+// }  
+//   render () {
+//     return (
+//       <div className="teamlist">
+//         <h2>Teams</h2>
+//         <ul className="sidebar-ul">
+//           {this.props.teams.map( team => (
+//             <TeamIndexItem className="team-list-item"
+//               key={team.id}
+//               team={team}
+//               getTeam={this.props.getTeam}
+//               deleteTeam={this.props.deleteTeam}
+//               />
+//           ))}
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+// export default TeamIndex
