2f55b439376b69021a4b73e82f217d66b66ca4d0
Brian van den Broek <vanden@gmail.com>
2018-02-09 14:42:45 -0800

    Add new users to all teams by default
    Not really the right thing. But the right thing right now.
    
    Ultimately, a better solution involving users joining groups needs to be
    in place.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,9 +2,14 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
+    
     if @user.save
       login(@user)
+
+      # Suboptimal solution to the problem that presently there is no
+      # way for users to join groups through the app. It is what there
+      # is time for, right now.
+      Team.all.each { |team| @user.teams << team }
       render "api/users/show"
     else
       render json: @user.errors.full_messages, status: 422
