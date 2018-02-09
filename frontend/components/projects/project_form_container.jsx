06b0adb653bae06ab11ed000144354669cc6a6bd
Brian van den Broek <vanden@gmail.com>
2018-02-09 15:36:57 -0800

    Improve code layout

--- frontend/components/projects/project_form_container.jsx
+++ frontend/components/projects/project_form_container.jsx
@@ -6,15 +6,15 @@ import { getProject, postProject, updateProject } from '../../actions/project_ac
 const mapStateToProps = (state, ownProps) => {
 
   let owner_id = state.session.currentUser.id
-  let project;
+  let project
   const formType = ownProps.match.path.split('/').pop()
 
+  // Rather than a ternary or an if else, an if and else if are used
+  // so that no unintended default case can arise.
   if (formType === "new") {
     const team_id = ownProps.match.params.team_id
     project = { name: "", description: "", team_id, owner_id  }
-  }
-
-  if (formType === "edit") {
+  } else if (formType === "edit") {
     project = state.entities.projects[ownProps.match.params.project_id]
   }
 
@@ -25,10 +25,8 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch, ownProps) => {
   const formType = ownProps.match.path.split('/').pop()
   let action
-  //  console.log(ownProps)
 
-  // Rather than ternary or if else, two ifs so no unintended default
-  // case can arise.
+  // As above; else if to block an accidental default
   if (formType === "new") {
     action = postProject
   }
@@ -38,11 +36,11 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     getProject: id => dispatch(getProject(id)),
     action: project => dispatch(action(project))
-  };
-};
+  }
+}
 
 
 export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(projectForm));
+)(projectForm))
