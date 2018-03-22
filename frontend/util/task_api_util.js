a9e2f5daccbed8d3838f99fc1fc13e4dc67f02b8
Brian van den Broek <vanden@gmail.com>
2018-03-22 16:04:01 -0700

    Add task api functions

--- frontend/util/task_api_util.js
+++ frontend/util/task_api_util.js
@@ -1,1 +1,41 @@
+export const getTask = (id) => {
+  return $.ajax({
+    url: `/api/tasks/${id}`,
+    method: 'GET'
+  })
+}
+
+
+export const getAllTasks = () => {
+  return $.ajax({
+    url: '/api/tasks',
+    method: 'GET'
+  })
+}
+
+
+export const postTask = (task) => {
+  return $.ajax({
+    url: '/api/tasks',
+    method: 'POST',
+    data: { task }
+  })
+}
+
+
+export const updateTask = (task) => {
+  return $.ajax({
+    url: `/api/tasks/${task.id}`,
+    method: 'PATCH',
+    data: { task }
+  })
+}
+
+
+export const deleteTask = (id) => {
+  return $.ajax({
+    url: `/api/tasks/${id}`,
+    method: 'DELETE'
+  })
+}
  
