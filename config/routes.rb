5fb19bb1a2fdb4e26c6056ff0fd10ee4d17227e2
Brian van den Broek <vanden@gmail.com>
2018-03-22 11:55:30 -0700

    Remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :update, :destroy, :show]
-    resource :session, only: [:create, :destroy, :show]
+    resource :session, only: [:create, :destroy]
     resources :teams, except: [:new, :edit]
     resources :team_memberships, only: [:create, :show, :destroy]
     resources :projects, except: [:new, :edit]
